4c2755c9ec5acb0aa34d4c5101a6aeb1edda44f5
krasteki <kiril.krastev@hashicorp.com>
2022-02-25 10:59:23 +0200

    correct_cycle_error

--- main.tf
+++ main.tf
@@ -50,40 +50,29 @@ resource "aws_instance" "web_app" {
 resource "aws_security_group" "sg_ping" {
   name = "Allow Ping"
 
-  ingress {
-    from_port       = -1
-    to_port         = -1
-    protocol        = "icmp"
-    security_groups = [aws_security_group.sg_8080.id]
-  }
 }
 
 resource "aws_security_group" "sg_8080" {
   name = "Allow 8080"
 
-  ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_ping.id]
-  }
 }
 
-resource "aws_security_group_rule" "allow_localhost_8080" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
-  security_group_id = aws_security_group.sg_8080.id
+resource "aws_security_group_rule" "allow_ping" {
+    type = "ingress"
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    security_group_id = aws_security_group.sg_ping.id
+    source_security_group_id = aws_security_group.sg_8080.id
 }
 
-resource "aws_security_group_rule" "allow_localhost_ping" {
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "icmp"
-  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
-  security_group_id = aws_security_group.sg_ping.id
+resource "aws_security_group_rule" "allow_8080" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_group_id = aws_security_group.sg_8080.id
+    source_security_group_id = aws_security_group.sg_ping.id
 }
+
 
